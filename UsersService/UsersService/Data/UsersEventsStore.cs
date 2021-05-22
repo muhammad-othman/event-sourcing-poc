e6c666035a144e21770524adbdcae41fbbb4d9bd
Muhammad Othman <muhammad.oth91@gmail.com>
2021-05-22 13:56:14 +0200

    add users subscriber

--- UsersService/UsersService/Data/UsersEventsStore.cs
+++ UsersService/UsersService/Data/UsersEventsStore.cs
@@ -1,15 +1,19 @@
 ﻿using MongoDB.Driver;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace UsersService.Data
 {
     public class UsersEventsStore : IUsersEventsStore
     {
-        private readonly IMongoCollection<UserEvent> usersEventsCollection;
+        private readonly IModel rabbitMQChannel;
 
+        private readonly IMongoCollection<UserEvent> usersEventsCollection;
         private static List<User> usersList = new List<User>();
         public IEnumerable<User> Users { get; private set; }
 
@@ -20,6 +24,20 @@ namespace UsersService.Data
 
             usersEventsCollection = db.GetCollection<UserEvent>("usersevents");
             Users = usersList;
+
+            var factory = new ConnectionFactory();
+
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            factory.HostName = "45.63.116.153";
+            factory.Port = 5672;
+
+            var connection = factory.CreateConnection();
+
+            rabbitMQChannel = connection.CreateModel();
+
+            rabbitMQChannel.ExchangeDeclare("users.exchange", ExchangeType.Fanout, true);
         }
 
         public User AddUser(User newUser)
@@ -69,12 +87,18 @@ namespace UsersService.Data
         private void SaveAndProcessEvent(UserEvent userEvent)
         {
             usersEventsCollection.InsertOne(userEvent);
+            
+            //Publish Events
 
             ProcessEvent(userEvent);
         }
 
         private void ProcessEvent(UserEvent userEvent)
         {
+            // TODO move to SaveAndProcessEvent 
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(userEvent));
+            rabbitMQChannel.BasicPublish("users.exchange", "", null, body);
+
             switch (userEvent.Type)
             {
                 case EventType.Create:
---
+++ UsersSubscriber/UsersSubscriber/Program.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsersSubscriber
+{
+    class Program
+    {
+
+        static List<User> usersList = new List<User>();
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            factory.HostName = "45.63.116.153";
+            factory.Port = 5672;
+
+
+            var connection = factory.CreateConnection();
+
+            var rabbitMQChannel = connection.CreateModel();
+
+            var queueName = "users.subscribe.queue";
+
+            rabbitMQChannel.QueueDeclare(queueName, true, false, false);
+            rabbitMQChannel.QueueBind(queueName, "users.exchange", "");
+
+            var consumer = new EventingBasicConsumer(rabbitMQChannel);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+
+                var body = e.Body.ToArray();
+                var jsonString = Encoding.UTF8.GetString(body);
+
+                var @event = JsonConvert.DeserializeObject<UserEvent>(jsonString);
+
+
+                Console.WriteLine("=================================");
+                Console.WriteLine("=================================");
+                Console.WriteLine($"Event Type: ${@event.Type}");
+                Console.WriteLine(@event.UserData);
+                Console.WriteLine("=================================");
+                Console.WriteLine("=================================");
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                switch (@event.Type)
+                {
+                    case EventType.Create:
+                        usersList.Add(@event.UserData);
+                        break;
+                    case EventType.Update:
+                        var oldUser = usersList.First(e => @event.UserData.Id == e.Id);
+
+                        oldUser.FirstName = @event.UserData.FirstName;
+                        oldUser.LastName = @event.UserData.LastName;
+                        oldUser.Email = @event.UserData.Email;
+
+                        break;
+                    case EventType.Delete:
+                        usersList.RemoveAll(e => @event.UserData.Id == e.Id);
+                        break;
+                }
+
+                rabbitMQChannel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+
+            rabbitMQChannel.BasicConsume(queueName, false, consumer);
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+    public class UserEvent
+    {
+        public Guid Id { get; set; }
+        public EventType Type { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public User UserData { get; set; }
+    }
+
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName} \nEmail: {Email}";
+        }
+    }
+
+    public enum EventType
+    {
+        Create,
+        Update,
+        Delete
+    }
+}
